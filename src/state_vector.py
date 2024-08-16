f8475a2ad580a4d623fb48c9b4a6df03dc2792ed
ali sarlak <ali.sarlak@protonmail.com>
2024-08-16 06:00:09 +0200

    file has been formatted

--- src/state_vector.py
+++ src/state_vector.py
@@ -1,9 +1,16 @@
 class State_Vector:
-    def __init__(self, y: float = 0.0, theta: float = 0.0, y_dot: float = 0.0, theta_dot: float = 0.0) -> None:
+    def __init__(self, y: float = 0.0,
+                 theta: float = 0.0,
+                 y_dot: float = 0.0,
+                 theta_dot: float = 0.0) -> None:
         self.y = y
         self.theta = theta
         self.y_dot = y_dot
         self.theta_dot = theta_dot
 
-    def __repr__(self) -> str:
-        return f"State_Vector(y={self.y}, testa={self.theta}, y_dot={self.y_dot}, theta_dot={self.theta_dot})"
+
+def __repr__(self) -> str:
+    return f"State_Vector(y={self.y}, " \
+        f"theta={self.theta}, " \
+        f"y_dot={self.y_dot}, " \
+        f"theta_dot={self.theta_dot})"
