7f446677f6d43de2c7497a19b2cf4ef6c00b31c7
ali sarlak <ali.sarlak@protonmail.com>
2024-08-23 23:22:55 +0200

    utilities to convert types

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from state_vector import State_Vector
+
+
+def sv2np(x: State_Vector):
+    return np.array([
+        [x.y],
+        [x.theta],
+        [x.y_dot],
+        [x.theta_dot],
+    ])
+
+
+def np2sv(x: np.array):
+    return State_Vector(x[0, 0],
+                        x[1, 0],
+                        x[2, 0],
+                        x[3, 0])
