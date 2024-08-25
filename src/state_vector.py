1db1122d130c83821ab6703993939a663b94dccc
ali sarlak <ali.sarlak@protonmail.com>
2024-08-25 19:20:55 +0200

    change theta to alpha for rocket orientation

--- src/state_vector.py
+++ src/state_vector.py
@@ -1,16 +1,16 @@
 class State_Vector:
     def __init__(self, y: float = 0.0,
-                 theta: float = 0.0,
+                 alpha: float = 0.0,
                  y_dot: float = 0.0,
-                 theta_dot: float = 0.0) -> None:
+                 alpha_dot: float = 0.0) -> None:
         self.y = y
-        self.theta = theta
+        self.alpha = alpha
         self.y_dot = y_dot
-        self.theta_dot = theta_dot
+        self.alpha_dot = alpha_dot
 
 
 def __repr__(self) -> str:
     return f"State_Vector(y={self.y}, " \
-        f"theta={self.theta}, " \
+        f"alpha={self.alpha}, " \
         f"y_dot={self.y_dot}, " \
-        f"theta_dot={self.theta_dot})"
+        f"alpha_dot={self.alpha_dot})"
