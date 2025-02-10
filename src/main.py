25ec9eb04e5d24855db36caf15c95d49d048226b
ali sarlak <ali.sarlak@protonmail.com>
2025-02-10 06:53:52 +0100

    increase font size for diagrams

--- src/main.py
+++ src/main.py
@@ -11,19 +11,17 @@ import time
 def main():
     print("Start ...")
 
-    # print(plt.style.available)
-
     fps = 50
-    mass = 30  # KG
+    mass = 30
 
     visualizer = Visualize(width=800, height=1000, fps=fps)
-    rocket_y = 500  # in pixels
+    rocket_y = 200  # in pixels
     # initial SS vector
-    #
+    #***********************************
     #   Do not set alpha > ±50
     #    abs(alpha) must be <=50
     #  300 <  x  < 500
-    #
+    #***********************************
     initial_state = State_Vector(x=250, y=rocket_y, alpha=radians(-70))
     print(initial_state)
 
@@ -113,22 +111,22 @@ def main():
 
         print("\n")
 
-        # if(current_time - start_time > 200):
-        time_stamp.append(current_time - start_time)
-        alpha_list.append(degrees(ps.rocket.state_vector.alpha))
-        nozzle_list.append(degrees(nozzle_angle))
-        doty_list.append(ps.rocket.state_vector.y_dot)
-        x_list.append(ps.rocket.state_vector.x)
-        dotalpha_list.append(degrees(ps.rocket.state_vector.alpha_dot))
-        thrust_list.append(abs(thrust))
+        if(current_time - start_time > 210):
+            time_stamp.append(current_time - start_time)
+            alpha_list.append(degrees(ps.rocket.state_vector.alpha))
+            nozzle_list.append(degrees(nozzle_angle))
+            doty_list.append(ps.rocket.state_vector.y_dot)
+            x_list.append(ps.rocket.state_vector.x)
+            dotalpha_list.append(degrees(ps.rocket.state_vector.alpha_dot))
+            thrust_list.append(abs(thrust))
 
 
-        p_alpha_list.append(degrees(predicted_state[2]))
-        p_doty_list.append(predicted_state[4])
-        p_x_list.append(predicted_state[0])
-        p_dotalpha_list.append(degrees(predicted_state[5]))
-        # if(current_time - start_time>235):
-        #     break
+            p_alpha_list.append(degrees(predicted_state[2]))
+            p_doty_list.append(predicted_state[4])
+            p_x_list.append(predicted_state[0])
+            p_dotalpha_list.append(degrees(predicted_state[5]))
+        if(current_time - start_time>245):
+            break
 
         visualizer.update()
         # running = False
@@ -141,15 +139,16 @@ def main():
 
     # Titles for each plot
     titles = [
-        "Alpha (degree)",
-        "DotAlpha (degree/sec)",
-        "DotY (m/s)",
-        "X (m)",
-        "Nozzle (degree)",
-        "Thrust (N)",
+        r"$\alpha$ (deg)",
+        r"$\dot{\alpha}$ (deg/sec)",
+        r"$\dot{Y}$ (m/s)",
+        r"$X$ (m)",
+        r"Nozzle (deg)",
+        r"Thrust (N)",
     ]
 
-    # Data lists to plot (each tuple contains original and p_ version)
+
+
     data_lists = [
         (alpha_list, p_alpha_list),
         (dotalpha_list, p_dotalpha_list),
@@ -161,14 +160,13 @@ def main():
 
     plt.style.use("seaborn-v0_8-deep")
 
-    # Create subplots
-    # fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(10, 8), constrained_layout=True)
+    colors = ["blue", "red"]
 
-    # Flatten axes for easy iteration (since it's a grid)
-    # axes = axes.flatten()
+    font = {'family' : 'Sans serif',
+            'weight' : 'bold',
+            'size'   : 14}
 
-    # Define colors for the two sets of data
-    colors = ["blue", "red"]  # Original data in blue, p_ data in red
+    plt.rc('font', **font)
 
     for i, ((data, p_data), title) in enumerate(zip(data_lists, titles)):
 
@@ -180,133 +178,21 @@ def main():
             ax.plot(time_stamp, p_data, label=f"{title} (Predicted)", linewidth=2, color=colors[1], linestyle="dashed")
 
         ax.set_title(title, fontsize=12, fontweight="bold")
-        ax.set_xlabel("Time (s)", fontsize=10)
-        ax.set_ylabel("Value", fontsize=10)
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Value")
+
 
         ax.grid(which="both")
         ax.grid(which="minor", alpha=0.2)
         ax.grid(which="major", alpha=0.5)
 
-        ax.legend()
+        ax.legend(fontsize=18)
 
         filename = f"{i}.png"
         plt.savefig(filename, dpi=600, bbox_inches="tight")
 
         plt.close(fig)
 
-    # Display the plot
-    # plt.suptitle(
-    #     "Smooth Line Charts of Variables vs Time", fontsize=16, fontweight="bold"
-    # )
-    # plt.show()
-
-
-    # # DRAW PLOTS
-
-    # # Titles for each plot
-    # titles = [
-    #     "Alpha (degree)",
-    #     "DotAlpha (degree/sec)",
-    #     "DotY (m/s)",
-    #     "X (m)",
-    #     "Nozzle (degree)",
-    #     "Thrust (N)",
-    # ]
-
-    # # Data lists to plot
-    # data_lists = [
-    #     alpha_list,
-    #     dotalpha_list,
-    #     doty_list,
-    #     x_list,
-    #     nozzle_list,
-    #     thrust_list,
-    # ]
-
-    # plt.style.use("seaborn-v0_8-deep")
-
-    # # Create subplots
-    # fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(10, 8), constrained_layout=True)
-
-    # # Flatten axes for easy iteration (since it's a grid)
-    # axes = axes.flatten()
-
-    # for i, (ax, data, title) in enumerate(zip(axes, data_lists, titles)):
-    #     # ax.set_facecolor("lightgrey")
-    #     ax.plot(time_stamp, data, label=title, linewidth=2, color="blue")
-
-    #     ax.set_title(title, fontsize=12, fontweight="bold")
-    #     ax.set_xlabel("Time (s)", fontsize=10)
-    #     ax.set_ylabel("Value", fontsize=10)
-
-    #     ax.grid(which="both")
-    #     ax.grid(which="minor", alpha=0.2)
-    #     ax.grid(which="major", alpha=0.5)
-
-    #     ax.legend()
-
-    # # Remove the last empty subplot
-    # if len(data_lists) < len(axes):
-    #     fig.delaxes(axes[-1])
-
-    # # Display the plot
-    # plt.suptitle(
-    #     "Smooth Line Charts of Variables vs Time", fontsize=16, fontweight="bold"
-    # )
-    # plt.show()
-
 
 if __name__ == "__main__":
     main()
-
-
-    # for i, (data, title) in enumerate(zip(data_lists, titles)):
-    #     fig, ax = plt.subplots(figsize=(10, 4))  # Create a new figure for each plot
-
-    #     ax.plot(time_stamp, data, label=title, linewidth=2, color="blue")
-    #     ax.set_title(title, fontsize=12, fontweight="bold")
-    #     ax.set_xlabel("Time (s)", fontsize=10)
-    #     ax.set_ylabel("Value", fontsize=10)
-
-    #     ax.grid(which="both")
-    #     ax.grid(which="minor", alpha=0.2)
-    #     ax.grid(which="major", alpha=0.5)
-
-    #     ax.legend()
-
-    #     # Save each figure separately
-    #     filename = f"{i}.png"
-    #     plt.savefig(filename, dpi=300, bbox_inches="tight")
-
-    #     plt.close(fig)
-
-
-    # # Create subplots
-    # fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(10, 8), constrained_layout=True)
-
-    # # Flatten axes for easy iteration (since it's a grid)
-    # axes = axes.flatten()
-
-    # for i, (ax, data, title) in enumerate(zip(axes, data_lists, titles)):
-    #     # ax.set_facecolor("lightgrey")
-    #     ax.plot(time_stamp, data, label=title, linewidth=2, color="blue")
-
-    #     ax.set_title(title, fontsize=12, fontweight="bold")
-    #     ax.set_xlabel("Time (s)", fontsize=10)
-    #     ax.set_ylabel("Value", fontsize=10)
-
-    #     ax.grid(which="both")
-    #     ax.grid(which="minor", alpha=0.2)
-    #     ax.grid(which="major", alpha=0.5)
-
-    #     ax.legend()
-
-    # # Remove the last empty subplot
-    # if len(data_lists) < len(axes):
-    #     fig.delaxes(axes[-1])
-
-    # # Display the plot
-    # plt.suptitle(
-    #     "Smooth Line Charts of Variables vs Time", fontsize=16, fontweight="bold"
-    # )
-    # plt.show()
